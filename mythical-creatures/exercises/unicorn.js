7d282288b08a3598c3977d50d33a2ab1d5f04e81
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-08-29 19:55:37 -0400

    add finished page of unicorn file

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,25 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    if(color === undefined) {
+      this.color = "white"
+    } else {
+      this.color = color;
+    }
   }
-}
+  isWhite() {
+    console.log(this.color);
+    if(this.color !== 'white') {
+      return false;
+    }
+  }
+  says(emotion) {
+    return `**;* ${emotion} *;**`;
+  }
+};
 
+// read test
+// ask yourself it will fail and why
+// run test
+// make it pass
 module.exports = Unicorn;
