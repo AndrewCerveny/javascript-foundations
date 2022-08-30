658d564fa69f5022f43e7590806ddbada873f820
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-08-30 16:23:20 -0400

    Add half of the Vampire done

---
+++ mythical-creatures/exercises/vampire.js
@@ -0,0 +1,31 @@
+class Vampire {
+  constructor(name,pet,thirsty) {
+    this.name = name
+    this.pet = 'bat'
+    this.thirsty = thirsty
+    
+  }
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Vampire;
