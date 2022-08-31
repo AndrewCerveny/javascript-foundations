53580e78afa20d750b5c807599944ef1c6a3b3d2
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-08-30 21:21:31 -0400

    Added a finished page of vampire

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -1,12 +1,22 @@
 class Vampire {
   constructor(name,pet,thirsty) {
     this.name = name
-    this.pet = 'bat'
-    this.thirsty = thirsty
-    
+    this.pet = pet || 'bat'
+    this.thirsty = true
+    this.ouncesDrank = 0
   }
-
+  drink() {
+    this.thirsty = false
+    if(this.ouncesDrank < 50) {
+      this.ouncesDrank += 10
+    } else {
+      return 'I\'m too full to drink anymore!'
+    }
+  }
 };
+
+
+
 
 
 
