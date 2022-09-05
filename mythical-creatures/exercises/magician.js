b4e4cb336d3a42171cd05c62270800ef1e1c14ba
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-09-04 23:19:10 -0400

    Add changes to key/pair values in constructor

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -1,52 +1,34 @@
 class Magician {
-  constructor({name:name, assistant:there, clothing:clothing,}) {
-    this.name = `The Great ${name}`
-    this.assistant = there
-    this.favoriteAccessory = clothing|| 'top hat'
-    this.confidencePercentage = 10
-
-
+  constructor({name, assistant, clothing}) {
+    this.name = `The Great ${name}`;
+    this.assistant = assistant;
+    this.favoriteAccessory = clothing || 'top hat';
+    this.confidencePercentage = 10;
   }
 
   performIncantation(magicWords) {
     if(magicWords ==='allakazaam') {
-      return 'ALLAKAZAAM!'
-    } else{
-      return 'ABRACADABRA!'
+      return 'ALLAKAZAAM!';
+    } else {
+      return 'ABRACADABRA!';
     }
   }
   performTrick() {
-    this.confidencePercentage += 10
+    this.confidencePercentage += 10;
     if(this.favoriteAccessory === 'top hat') {
-      return 'PULL RABBIT FROM TOP HAT'
+      return 'PULL RABBIT FROM TOP HAT';
     } else {
-      return 'PULL DOVE FROM SLEEVE'
+      return 'PULL DOVE FROM SLEEVE';
     }
   }
   performShowStopper() {
   if(this.confidencePercentage >= 100 && this.assistant === true) {
-      return 'WOW! The magician totally just sawed that person in half!'
+      return 'WOW! The magician totally just sawed that person in half!';
     } else {
-        return 'Oh no, this trick is not ready!'
+        return 'Oh no, this trick is not ready!';
     }
-  }
+  };
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 module.exports = Magician;
