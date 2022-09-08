f1e945adda30004cc4937cfe4cd174ca2691e59d
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-09-08 11:47:50 -0400

    Pass snowman tests

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,18 @@
 class Snowman {
-
+  constructor(details) {
+    this.carrots = details.carrots;
+    this.coal = details.coal;
+    this.buttons = details.buttons;
+    this.snowballs = details.snowballs;
+    this.magicHat = details.magicHat || false
+  }
+  canWearMagicHat() {
+    if (this.coal < 2 || this.buttons < 5 || this.carrots < 1 || this.snowballs < 2) {
+    this.magicHat = false
+  } else {
+    this.magicHat = true
+    }
+  }
 }
 
 module.exports = Snowman;
