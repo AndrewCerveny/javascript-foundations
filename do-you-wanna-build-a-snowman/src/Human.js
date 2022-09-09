902e028242be87a95f7f5a4bd95cd8ca7de80a4f
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-09-09 10:37:39 -0400

    WIP need to ask for help to finish

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,33 @@
 var Snowman = require('./Snowman');
 
 class Human {
+  constructor(name,) {
+    this.name = name;
+    this.wantsToBuildASnowman = true;
+    this.materials = {
+      snowballs:0,
+      coal:0,
+      buttons:0,
+      carrots:0,
+    };
+  }
+  gatherMaterials(bodyPart,amount) {
+    this.materials[bodyPart] += amount
+  }
+  buildASnowman() {
+    var frosty = new Snowman(this.materials);
+    return frosty
+  }
+  placeMagicHat() {
+    function buildASnowman() {
+      console.log(frosty)
+    }
+  }
+};
 
-}
-
+// ; && Snowman.magicHat = true){
+//       'Woah, this snowman is coming to life!'
+//   } else {
+//     return 'I guess I didn\'t build it correctly.'
+// return'Woah, this snowman is coming to life!'
 module.exports = Human;
