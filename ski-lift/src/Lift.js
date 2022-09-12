59be043c6d7b37bc3280c169e5c9e6a245e15095
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-09-11 20:26:38 -0400

    Finished skier test

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -1,7 +1,10 @@
 var Skier = require('./Skier');
 
 class Lift {
-  
+  constructor(name){
+    this.name = name;
+  }
+
 }
 
 module.exports = Lift;
--- ski-lift/src/Skier.js
+++ ski-lift/src/Skier.js
@@ -1,5 +1,22 @@
 class Skier {
-
-}
+constructor(name,ticketHolder) {
+  this.name = name;
+  this.hasLiftTicket = ticketHolder || 'true';
+  this.skillLevel = 1
+  this.nextSlope = 'green circle'
+  }
+takeLesson(){
+  this.skillLevel += 1
+  }
+pickSlope() {
+  if(this.skillLevel > 4 ) {
+    this.nextSlope = 'black diamond'
+  } else if (this.skillLevel === 3 || 4) {
+    this.nextSlope = 'blue square'
+  } else {
+    this.skillLevel = 'green circle'
+    }
+  }
+};
 
 module.exports = Skier;
