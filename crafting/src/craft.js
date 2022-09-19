bb1fcd3e50fdef65cab8c637ad0adf144820de00
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-09-18 21:47:12 -0400

    finished crafts test

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,19 @@
 class Craft {
+ constructor(artWork) {
+   this.name = artWork.type;
+   this.materials = artWork.materials; // array w/2obj
+   this.completed = false
+  }
+  completeCraft(){
+    this.completed = true;
+    return "All done! It looks great!"
+  }
 
+  calculateProjectTotal() {
+  var canvasCost = this.materials[0].price * this.materials[0].amount
+  var paintCost = this.materials[1].price * this.materials[1].amount
+  return canvasCost + paintCost
+  }
 }
 
 module.exports = Craft;
