328cb788897378b97afe2ec11ebfc5c58bf76f62
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-09-19 21:15:00 -0400

    Add finished lunchBox test suite

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,29 @@
 class LunchBox {
+  constructor(boxDetails){
+    this.owner = boxDetails.owner;
+    this.material = boxDetails.madeOf;
+    this.shape = boxDetails.shape;
+    this.color = boxDetails.color;
+    this.snacks = [];
 
+  }
+
+  acquireSnack(snackType) {
+    this.snacks.push(snackType)
+    for( var i = 0; i < this.snacks.length; i++) {
+      return this.snacks[i].isInLunchBox = true;
+    }
+  }
+
+  findHealthySnacks() {
+    var healthySnacks = [];
+
+    for(var i = 0; i < this.snacks.length; i++) {
+      if(this.snacks[i].checkForHealthy()) {
+        healthySnacks.push(this.snacks[i].type);
+      }
+    } return healthySnacks
+  }
 }
 
 module.exports = LunchBox;
